ac2589d499f0e8de95265746125a662322de52aa
Bui Sy Nguyen <nguyenbs@gmail.com>
2022-12-01 20:44:54 +0700

    Update Facebook footer item

--- app/Theme/house3d/footer.php
+++ app/Theme/house3d/footer.php
@@ -1,7 +1,6 @@
 <?php
 global $footerSetting;
 ?>
-<div id="fb-root"></div>
 <footer >
     <div class="container-fluid">
         <div class="footer_top">
@@ -271,6 +270,7 @@ global $footerSetting;
 </script>
 
 <!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
 <script>(function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
